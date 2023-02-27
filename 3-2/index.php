d41d341ecd4f1423d394b30f5bc27c2796b70154
salsakazu <salsakazu@yahoo.co.jp>
2023-02-27 13:53:42 +0900

    Update index.php

--- 3-2/index.php
+++ 3-2/index.php
@@ -6,7 +6,13 @@ $fruit = array(
     '桃' => 150
 );
 
+$num = [リンゴの個数, みかんの個数, 桃の個数];
+
 forreach ($fruit as $key => $value)[
+    function getPrice($price, $num) {
+        $total = ?????? ; /* 単価と個数から合計値を計算 */
+        return $total;  /* 合計値を返す */
+    }
     echo $key.'は'$value.'円です。';
 ]
 
