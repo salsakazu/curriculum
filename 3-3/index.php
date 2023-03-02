c704d56f1916474d638ad4b921d75ed7006ce1fb
salsakazu <salsakazu@yahoo.co.jp>
2023-03-02 13:55:45 +0900

    Create index.php

---
+++ 3-3/index.php
@@ -0,0 +1,9 @@
+<?php  
+
+<form action="result.php" method="post">   
+'0~9の数字を使って好きな数字の羅列を入力してください。' 
+            <input type="text" name="num" />
+            <input type="submit" value="占う" />
+</form>
+
+?>
