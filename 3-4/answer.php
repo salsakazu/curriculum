16bf3a1dff30361ee82cd496ad6ce8aadf2e02e0
salsakazu <salsakazu@yahoo.co.jp>
2023-03-10 17:09:24 +0900

    Update answer.php

--- 3-4/answer.php
+++ 3-4/answer.php
@@ -2,14 +2,18 @@
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
 
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
-
+?>
 
 <p><!--POST通信で送られてきた名前を表示-->さんの結果は・・・？</p>
 <?php
+$name = $_POST['name'];
+echo $name;
+?>
 
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
 
-$name = $_POST['name'];
+
+
 
 $Ans1 = $_POST['Ans1'];
 
@@ -23,41 +27,45 @@ $correctAns2 = $_POST['correctAns2'];
 
 $correctAns3 = $_POST['correctAns3'];
 
-?>
 
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-if($question === 80){
+<?php
+if ($Ans1 === $correctAns1) {
 
 echo "正解";
 
-}elseif($result == 80){
+}else{
 
 echo "残念・・・";
 
 }
+?>
 
 
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-if($question == HTML){
+<?php
+if($Ans2 === $correctAns2){
 
     echo "正解";
     
-    }elseif($result == HTML){
+    }else{
     
     echo "残念・・・";
     
     }
+?>
 
 
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-if($question == join){
+<?php
+if($Ans3 === $correctAns3){
 
     echo "正解";
     
-    }elseif($result == join){
+    }else{
     
     echo "残念・・・";
     
