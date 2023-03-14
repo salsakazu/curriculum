06f6ad68a444f4ab1ca432c327cf8773d39ac4ce
salsakazu <salsakazu@yahoo.co.jp>
2023-03-14 10:50:48 +0900

    Update answer.php

--- 3-4/answer.php
+++ 3-4/answer.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="style.css">
+
 <?php 
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
 
@@ -7,18 +9,14 @@ $name = $_POST['name'];
 <p><!--POST通信で送られてきた名前を表示--><?php echo $name;?>さんの結果は・・・？</p>
 
 
-//[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
-
-
-
-
+<php?
 $Ans1 = $_POST['Ans1'];
 $Ans2 = $_POST['Ans2'];
 $Ans3 = $_POST['Ans3'];
 $correctAns1 = $_POST['correctAns1'];
 $correctAns2 = $_POST['correctAns2'];
 $correctAns3 = $_POST['correctAns3'];
-
+?>
 
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
