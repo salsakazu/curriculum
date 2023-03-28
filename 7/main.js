88f4931018dcdb992014a4a2e4a8fa1ca85794fe
salsakazu <salsakazu@yahoo.co.jp>
2023-03-28 18:31:14 +0900

    Update main.js

--- 7/main.js
+++ 7/main.js
@@ -1,13 +1,27 @@
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+
+for (let i = 0; i < 22; i++){
+    isEven(numbers[i]);
+}
+
+function isEven(numbers) {
+    if (numbers % 2 === 0 ){
+    console.log( numbers + 'は偶数です');
+}
+}
+
+
+
 class Car {
     constructor(gas, number) {
     this.gas = gas;
     this.number = number;
 }
+}
 
-getNumGas() {
+    getNumGas(); {
     console.log(`ガソリンは${this.gas}です。ナンバーは${this.number}です`);
 }
 
 let carinfo = new Car(20, 1234);
-carinfo.getNumGas();
-}
+carinfo.getNumGas();    
